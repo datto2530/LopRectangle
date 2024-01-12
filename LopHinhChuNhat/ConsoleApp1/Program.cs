49b462fd84bac5773922736a7862f4c13a4ca323
Dattobias <152405844+datto2530@users.noreply.github.com>
2024-01-12 15:03:29 +0700

    da xong

---
+++ LopHinhChuNhat/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Drawing;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap chieu rong hinh chu nhat");
+            double width = double.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap chieu cao hinh chu nhat");
+            double height = double.Parse(Console.ReadLine());
+            Rectangle rectangle = new Rectangle(width, height);
+            rectangle.PrintfArea();
+            rectangle.PrintPerimeter();
+        }
+    }
+    class Rectangle
+    {
+        double width, height;
+        public Rectangle()
+        {
+
+        }
+        public Rectangle(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+        public void PrintfArea()
+        {
+            Console.WriteLine("Dien tich la: " + this.width * this.height);
+        }
+        public void PrintPerimeter()
+        {
+            Console.WriteLine("Chu vi la: " + (this.width + this.height) * 2);
+        }
+    }
+}
