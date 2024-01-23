49cbb63d1d1de91078bc8eaa857a978b36a835c8
Dattobias <152405844+datto2530@users.noreply.github.com>
2024-01-23 21:59:08 +0700

    da xong

---
+++ Rectangle/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Nhap chieu rong:");
+            double width = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap chieu cao:");
+            double height = Double.Parse(Console.ReadLine());
+            Rectangle rectangle = new Rectangle(width, height);
+            Console.WriteLine("Hinh chu nhat cua ban \n" + rectangle.Display());
+            Console.WriteLine("Chu vi hinh chu nhat: " + rectangle.GetPerimeter());
+            Console.WriteLine("Dien tich hinh chu nhat: " + rectangle.GetArea());
+            Console.ReadKey();
+        }
+
+    }
+
+    public class Rectangle
+    {
+        double width, height;
+
+        public Rectangle()
+        {
+        }
+
+        public Rectangle(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public double GetArea()
+        {
+            return this.width * this.height;
+        }
+
+        public double GetPerimeter()
+        {
+            return (this.width + this.height) * 2;
+        }
+
+        public String Display()
+        {
+            return "Hinh chu nhat{" + "Rong =" + width + ", Cao =" + height + "}";
+        }
+        
+    }
+}
